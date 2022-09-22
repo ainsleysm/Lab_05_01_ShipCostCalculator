d1fa20c5be9ef0a2b0da08e7b4f2ebfa819b8d86
ainsleysm <smithainsley21@gmail.com>
2022-09-22 15:15:40 -0400

    Commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        double price = 90.05;
+        double price = 100.05;
 
         if (price >= 100) {
             System.out.println("Shipping cost is: $0.00");
